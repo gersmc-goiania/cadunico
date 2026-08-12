182dc9275c251db9d9431c076f96a3fadffe9729
gersmc-goiania <gerenciamediacomplexidade@gmail.com>
2026-08-12 17:55:00 -0300

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@
    ========================================================================== */
 window.APP_CONFIG = {
   // Client ID OAuth criado no Google Cloud Console (tela "Sign in with Google").
-  GOOGLE_CLIENT_ID: 'COLOQUE_AQUI_SEU_CLIENT_ID.apps.googleusercontent.com',
+  GOOGLE_CLIENT_ID: '339683006481-vuutlu0ke0si7hodhnd5dund5c7iivdg.apps.googleusercontent.com',
 
   // URL do Web App do Apps Script (termina em /exec), copiada ao publicar o Code.gs.
-  APPS_SCRIPT_URL: 'COLOQUE_AQUI_A_URL_DO_APPS_SCRIPT',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwvHYPX70AJDVO9HrC_0vxRKuWl7gvsNDz7guUUpgVLcP8aRajVZ-TLktWpq5Uq1KIL/exec',
 };
